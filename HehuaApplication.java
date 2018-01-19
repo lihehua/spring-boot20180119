aaa49346b337be5e4af9ceadd473d5c451c407dc
lihehua <1246338384@qq.com>
2018-01-19 09:54:34 +0800

    Add files via upload

---
+++ HehuaApplication.java
@@ -0,0 +1,23 @@
+package com.hehua.boot;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class HehuaApplication {
+
+    @Value("${test.text.hello}")
+    private String sayText;
+
+	@RequestMapping("/")
+	public String methodOne(){
+	    return "I'm here : methodOne!!!"+sayText+"乱码！";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(HehuaApplication.class, args);
+	}
+}
